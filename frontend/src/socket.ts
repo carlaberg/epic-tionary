3d820a44ab81856bac1e2808bc2a617b24abc519
caberg <carl.aberg@volvocars.com>
2024-12-25 00:56:16 +0100

    Add build and strart scripts

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -4,9 +4,7 @@ import {
   ClientToServerEvents,
 } from "../../shared/types/socket-io.types";
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:4001";
+const URL = process.env.NEXT_PUBLIC_SOCKET_URL;
 
 export const createSocket = async ({
   getToken,
@@ -17,7 +15,7 @@ export const createSocket = async ({
   const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
     URL as string,
     {
-      autoConnect: false, 
+      autoConnect: false,
       auth: {
         token,
       },
