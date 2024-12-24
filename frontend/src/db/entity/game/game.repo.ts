d482aca2b2d99eeabb88394b6723ebc948f035ac
caberg <carl.aberg@volvocars.com>
2024-12-24 11:33:41 +0100

    Remove connection log

--- frontend/src/db/entity/game/game.repo.ts
+++ frontend/src/db/entity/game/game.repo.ts
@@ -8,8 +8,6 @@ async function createGame(gameData?: Partial<Game>) {
   const playerRepo = connection.getRepository(Player);
   const gameRepo = connection.getRepository(Game);
 
-  console.log("connection", connection);
-
   const loggedInUser = await getCurrentUser();
   console.log("logged in user", loggedInUser);
   const game = await gameRepo.save(
